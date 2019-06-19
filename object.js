0c24e72dc5299c4f3f2728a83bd45d3b20307dcd
havus <havus.it@gmail.com>
2019-06-19 17:22:23 +0700

    back up

---
+++ object.js
@@ -0,0 +1,8 @@
+let arr = ['a', 'b', 'c', 'd', 'e'];
+let num = '123';
+
+for (let number of arr) {
+  console.log(number);
+}
+
+// console.log(typeof(num));
---
+++ snippetVscode.js
@@ -0,0 +1,41 @@
+// {
+// 	"FormEl": {
+// 		"prefix": "formel",
+// 		"body": [
+// 			"<label for=\"$1\">$2</label>",
+// 			"<input type=\"${3:text}\" name=\"$1\" id=\"$1\">"
+// 		],
+// 		"description": "Form Element"
+// 	},
+// 	// PHP Snippet
+// 	"Debug Pre": {
+// 		"prefix": "epre",
+// 		"body": [
+// 			"echo \"<pre>\".print_r(${1:\\$var},1).\"</pre>\";"
+// 		],
+// 		"description": "Debug with Pre"
+// 	},
+// 	"Console Log": {
+// 		"prefix": "clog",
+// 		"body": [
+// 			"console.log($1);"
+// 		],
+// 		"description": "console log"
+// 	},
+// 	"Delimiter PHP": {
+// 		"prefix": "php",
+// 		"body": [
+// 			"<?php $1 ?>"
+// 		],
+// 		"description": "delimiter php"
+// 	},
+// 	"Forr Loop": {
+// 		"prefix": "forloop",
+// 		"body": [
+// 			"for (let i = $1; i ${2:<}; i++) {",
+// 			"	${3:code}",
+// 			"}"
+// 		],
+// 		"description": "for loop"
+// 	}
+// }
